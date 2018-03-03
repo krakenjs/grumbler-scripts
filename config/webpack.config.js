73884235b0cfa6a84c850c6f1df33b7f347f0570
Daniel Brain <dbrain@paypal.com>
2018-03-02 17:18:34 -0800

    Export webpack function

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,7 +24,7 @@ type WebpackConfigOptions = {
     vars? : { [string] : mixed }
 };
 
-function getWebpackConfig({ filename, modulename, minify = false, options = {}, vars = {} } : WebpackConfigOptions) : Object {
+export function getWebpackConfig({ filename, modulename, minify = false, options = {}, vars = {} } : WebpackConfigOptions) : Object {
 
     vars = {
         ...DEFAULT_VARS,
@@ -111,30 +111,3 @@ function getWebpackConfig({ filename, modulename, minify = false, options = {}, 
         ...options
     };
 }
-
-export let WEBPACK_CONFIG = getWebpackConfig({
-    filename:   `${ FILE_NAME }.js`,
-    modulename: MODULE_NAME
-});
-
-export let WEBPACK_CONFIG_MIN = getWebpackConfig({
-    filename:   `${ FILE_NAME }.min.js`,
-    modulename: MODULE_NAME,
-    minify:     true,
-    vars:       {
-        __MIN__: true
-    }
-});
-
-export let WEBPACK_CONFIG_TEST = getWebpackConfig({
-    filename:   `${ FILE_NAME }.js`,
-    modulename: MODULE_NAME,
-    options:    {
-        devtool: 'inline-source-map'
-    },
-    vars: {
-        __TEST__: true
-    }
-});
-
-export default [ WEBPACK_CONFIG, WEBPACK_CONFIG_MIN ];
