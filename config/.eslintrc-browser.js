51a79437338c64def9ef5dc503676edc2ee0756e
Daniel Brain <dbrain@paypal.com>
2018-04-03 17:16:45 -0700

    Fix path to eslintrc

--- config/.eslintrc-browser.js
+++ config/.eslintrc-browser.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 module.exports = {
-    'extends': './node_modules/grumbler-scripts/config/.eslintrc.js',
+    'extends': './.eslintrc.js',
 
     rules: {
         'import/no-dynamic-require': 'error',
