8865f921a9fb2447b6ff16168ac3575aa35e7b7d
Daniel Brain <dbrain@paypal.com>
2018-07-10 15:27:18 -0700

    Turn off escape capitalization rule

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -319,7 +319,7 @@ module.exports = {
         'unicorn/no-process-exit': 'error',
         'unicorn/throw-new-error': 'error',
         'unicorn/number-literal-case': 'error',
-        'unicorn/escape-case': 'error',
+        'unicorn/escape-case': 'off',
         'unicorn/no-array-instanceof': 'error',
         'unicorn/no-new-buffer': 'error',
         'unicorn/no-hex-escape': 'error',
