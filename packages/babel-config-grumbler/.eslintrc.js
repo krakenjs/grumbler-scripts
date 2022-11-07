4a0e6c73951685d0e41c45c722f6f61bd4fbd401
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-11-07 09:27:52 -0600

    properly name eslint configs

--- packages/babel-config-grumbler/.eslintrc.js
+++ packages/babel-config-grumbler/.eslintrc.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-  extends: "@krakenjs/eslint-config-grumbler/eslint-node",
+  extends: "@krakenjs/eslint-config-grumbler/eslintrc-node",
   rules: {
     "flowtype/require-valid-file-annotation": "off",
   },
