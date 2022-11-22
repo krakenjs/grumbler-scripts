925e2fdf939f308fd3ffbf4fb3e5c08b07d5f2c6
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2022-11-22 12:37:19 -0600

    feat: add webpack typescript types

---
+++ packages/webpack-config-grumbler/index.d.ts
@@ -0,0 +1,28 @@
+import type webpack from "webpack";
+
+type WebpackConfigOptions = {
+  context?: string;
+  entry?: string | readonly string[];
+  filename?: string;
+  modulename?: string;
+  minify?: boolean;
+  test?: boolean;
+  options?: Object;
+  vars?: Record<string, unknown>;
+  alias?: Record<string, string>;
+  libraryTarget?: string;
+  web?: boolean;
+  debug?: boolean;
+  env?: string;
+  path?: string;
+  sourcemaps?: boolean;
+  cache?: boolean;
+  analyze?: boolean;
+  dynamic?: boolean;
+  babelConfig?: string;
+  publicPath?: string;
+};
+
+declare const getWebpackConfig: (
+  arg0: WebpackConfigOptions
+) => webpack.Configuration;
